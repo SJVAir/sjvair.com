25609901ef3042e902464093ff9f90c018a7f9a4
Derek Payton <derek.payton@gmail.com>
2019-05-15 03:30:22 -0700

    Test API endpoints, along with code updates to get tests passing. Also, a README!

--- aqm/apps/sensors/tests.py
+++ aqm/apps/sensors/tests.py
@@ -1,7 +1,14 @@
 import json
+import random
+
+from datetime import timedelta
+from decimal import Decimal
 
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
+from django.utils import timezone
+
+from resticus.encoders import JSONEncoder
 
 from . import views
 from .models import Sensor, SensorData
@@ -12,7 +19,69 @@ sensor_data = views.SensorData.as_view()
 
 
 def streaming_json(stream):
+    '''
+        Given a JSON stream (StreamingHttpResponse.streaming_content),
+        parse the JSON and return the data structure.
+    '''
     return json.loads(''.join([b.decode('utf8') for b in stream]))
+
+
+def random_trend(low, high, num, reverse=False):
+    # Not sure if this is an alright way to generate
+    # a pseudo-random trend-y list, but we'll see...
+    data = []
+    value = random.randint(0, high - random.randint(0, high / 2))
+    for x in range(num):
+        value = int(round(random.triangular(low, high, value)))
+        data.append(value)
+    return sorted(data, reverse=reverse)
+
+
+def fake_sensor_payload():
+    '''
+        Helper to generate a pseudo-random payload object
+    '''
+    pm2_trend = random_trend(0, 700, 3)
+    pm2_std = list(zip(('pm10_standard', 'pm25_standard', 'pm100_standard'), pm2_trend))
+    pm2_env = list(zip(
+        ('pm10_env', 'pm25_env', 'pm100_env'),
+        [x + random.randint(-10, 10) for x in pm2_trend]
+    ))
+
+    particle_trend = random_trend(0, 700, 6, reverse=True)
+    particles = list(zip((
+        'particles_03um',
+        'particles_05um',
+        'particles_10um',
+        'particles_25um',
+        'particles_50um',
+        'particles_100um'
+    ), particle_trend))
+
+    return dict(
+        celcius=random.randint(100, 400) / 10, # 10-40C (50-104F)
+        humidity=random.randint(300, 700) / 10, # 30-70% RH
+        barometric_pressure=0, # TODO
+        voc=200, # TODO
+        pm2=dict(
+            a=dict(pm2_std + pm2_env + particles),
+            # Same data, with some slight random variation
+            b={k: v + random.randint(-10, 10) for k, v in (pm2_std + pm2_env + particles)},
+        )
+    )
+
+
+def fake_sensor_data(sensor, payload=None, process=True, save=True):
+    data = SensorData(
+        sensor=sensor,
+        position=sensor.position,
+        payload=payload or fake_sensor_payload(),
+    )
+    if process:
+        data.process()
+    if save:
+        data.save()
+    return data
 
 
 class SensorTests(TestCase):
@@ -22,7 +91,7 @@ class SensorTests(TestCase):
         self.factory = RequestFactory()
         self.sensor = Sensor.objects.get(name='RAHS')
 
-    def test_sensor_list(self):
+    def test_get_sensor_list(self):
         url = reverse('sensors:sensor-list')
         request = self.factory.get(url)
         response = sensor_list(request)
@@ -31,7 +100,7 @@ class SensorTests(TestCase):
         content = streaming_json(response.streaming_content)
         assert content['data'][0]['id'] == str(self.sensor.pk)
 
-    def test_sensor_detail(self):
+    def test_get_sensor_detail(self):
         url = reverse('sensors:sensor-detail', kwargs={'sensor_id': self.sensor.pk})
         request = self.factory.get(url)
         response = sensor_detail(request, sensor_id=self.sensor.pk)
@@ -39,3 +108,31 @@ class SensorTests(TestCase):
 
         content = json.loads(response.content)
         assert content['data']['id'] == str(self.sensor.pk)
+
+    def test_get_sensor_data(self):
+        now = timezone.now()
+        for x in range(10):
+            data = fake_sensor_data(sensor=self.sensor, save=False)
+            data.timestamp = now - timedelta(minutes=10 * x)
+            data.save()
+
+        url = reverse('sensors:sensor-data', kwargs={'sensor_id': self.sensor.pk})
+        request = self.factory.get(url)
+        response = sensor_data(request, sensor_id=self.sensor.pk)
+        assert response.status_code == 200
+
+        content = streaming_json(response.streaming_content)
+        assert len(content['data']) == self.sensor.data.count()
+
+    def test_create_sensor_data(self):
+        payload = fake_sensor_payload()
+        url = reverse('sensors:sensor-data', kwargs={'sensor_id': self.sensor.pk})
+        request = self.factory.post(url, {'payload': payload}, content_type='application/json')
+        response = sensor_data(request, sensor_id=self.sensor.pk)
+        assert response.status_code == 200
+
+        content = streaming_json(response.streaming_content)
+        assert content['data']['sensor'] == str(self.sensor.pk)
+
+
+
--- aqm/apps/sensors/urls.py
+++ aqm/apps/sensors/urls.py
@@ -7,5 +7,5 @@ app_name = 'sensors'
 urlpatterns = [
     path('', views.SensorList.as_view(), name='sensor-list'),
     path('<str:sensor_id>/', views.SensorDetail.as_view(), name='sensor-detail'),
-    # path('<str:sensor_id>/data/', views.SensorDataList.as_view(), name='sensor-data'),
+    path('<str:sensor_id>/data/', views.SensorData.as_view(), name='sensor-data'),
 ]
