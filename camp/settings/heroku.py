ee254eb889c8550bce3b273de922529fcac63ad3
dmpayton <derek.payton@gmail.com>
2019-06-27 16:46:54 -0700

    Initial heroku config.

---
+++ camp/settings/heroku.py
@@ -0,0 +1,33 @@
+import django_heroku
+
+from .common import *
+
+# django_heroku.settings(locals())
+
+# GIS paths
+
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+
+# https
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+# Huey
+
+# HUEY['always_eager'] = False
+
+if DEBUG:
+    INSTALLED_APPS.extend([
+        'debug_toolbar'
+    ])
+
+    MIDDLEWARE.extend([
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ])
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': lambda request: True
+    }
