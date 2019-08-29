e95a406e12c45c26e843fc952d6fe9c628ba67b7
dmpayton <derek.payton@gmail.com>
2019-08-28 19:49:51 -0700

    Lay the ground work for the sensor detail display.

---
+++ assets/map/components/SensorDetail.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="sensor-detail">
+  {{ sensor.name }}
+</div>
+</template>
+
+<script>
+export default {
+  name: 'sensor-detail',
+  props: {
+    sensor: Object
+  },
+  methods: {
+  }
+}
+</script>
--- assets/map/main.js
+++ assets/map/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 Vue.use(VueResource);
 
 Vue.config.productionTip = false
+Vue.http.options.root = 'http://localhost:8000';
+// Vue.http.options.root = 'https://ccac-camp-api.herokuapp.com';
 
 new Vue({
   render: h => h(App),
