a60ca733043751bc81fd979a2798a439545cd3e5
dmpayton <derek.payton@gmail.com>
2019-09-02 05:55:13 -0700

    Initial charts with Apex Charts.

--- assets/map/main.js
+++ assets/map/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+// import AsyncComputed from 'vue-async-computed'
 import VueResource from 'vue-resource'
 import App from './App.vue'
 
+// Vue.use(AsyncComputed);
 Vue.use(VueResource);
 
 Vue.config.productionTip = false
-// Vue.http.options.root = '/api/1.0/';
-Vue.http.options.root = 'https://ccac-camp-api.herokuapp.com/api/1.0/';
+Vue.http.options.root = '/api/1.0/';
+// Vue.http.options.root = 'https://ccac-camp-api.herokuapp.com/api/1.0/';
 
 new Vue({
   render: h => h(App),
