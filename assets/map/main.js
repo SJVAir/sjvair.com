d40af5ef3666e908c404c283c1705e92428c5874
dmpayton <derek.payton@gmail.com>
2019-09-01 19:34:46 -0700

    Add ability to view a specific stat on the map.

--- assets/map/main.js
+++ assets/map/main.js
@@ -5,8 +5,8 @@ import App from './App.vue'
 Vue.use(VueResource);
 
 Vue.config.productionTip = false
-Vue.http.options.root = '/api/1.0/';
-// Vue.http.options.root = 'https://ccac-camp-api.herokuapp.com/api/1.0/';
+// Vue.http.options.root = '/api/1.0/';
+Vue.http.options.root = 'https://ccac-camp-api.herokuapp.com/api/1.0/';
 
 new Vue({
   render: h => h(App),
