0c144e7f3df1afcef172e58fb3ee6f04d26bc49f
dmpayton <derek.payton@gmail.com>
2020-06-24 01:34:11 -0700

    Refactor monitor admin to be more DRY.

--- camp/apps/monitors/admin.py
+++ camp/apps/monitors/admin.py
@@ -1,3 +1,53 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.template.defaultfilters import floatformat
 
-# Register your models here.
+
+class MonitorAdmin(admin.OSMGeoAdmin):
+    list_display = ['name', 'last_updated', 'temperature', 'humidity', 'pm10', 'pm25', 'pm100']
+    fields = ['name', 'location', 'position']
+
+    def get_queryset(self, request):
+        return (super()
+            .get_queryset(request)
+            .select_related('latest')
+        )
+
+    def last_updated(self, instance):
+        if instance.latest:
+            return instance.latest.timestamp
+        return ''
+
+    def temperature(self, instance):
+        if instance.latest is None:
+            return ''
+
+        temps = []
+        if instance.latest.fahrenheit:
+            temps.append(f'{intcomma(floatformat(instance.latest.fahrenheit, 1))}°F')
+        if instance.latest.celcius:
+            temps.append(f'{intcomma(floatformat(instance.latest.celcius, 1))}°C')
+        return ' / '.join(temps)
+
+    def humidity(self, instance):
+        if instance.latest and instance.latest.humidity:
+            return f'{int(round(instance.latest.humidity))}%'
+        return ''
+
+    def pm10(self, instance):
+        if instance.latest:
+            return instance.latest.pm10_env or ''
+        return ''
+    pm10.short_description = 'PM1.0'
+
+    def pm25(self, instance):
+        if instance.latest:
+            return instance.latest.pm25_env or ''
+        return ''
+    pm25.short_description = 'PM2.5'
+
+    def pm100(self, instance):
+        if instance.latest:
+            return instance.latest.pm100_env or ''
+        return ''
+    pm100.short_description = 'PM10.0'
--- camp/apps/monitors/purpleair/admin.py
+++ camp/apps/monitors/purpleair/admin.py
@@ -2,6 +2,7 @@ from django.contrib.gis import admin
 from django.contrib.humanize.templatetags.humanize import intcomma
 from django.template.defaultfilters import floatformat
 
+from camp.apps.monitors.admin import MonitorAdmin
 from camp.apps.monitors.purpleair.forms import PurpleAirAddForm
 from camp.apps.monitors.purpleair.models import PurpleAir
 from camp.apps.monitors.purpleair.tasks import import_monitor_data
@@ -9,21 +10,20 @@ from camp.utils.forms import DateRangeForm
 
 
 @admin.register(PurpleAir)
-class PurpleAirAdmin(admin.OSMGeoAdmin):
-    list_display = ['name', 'purple_id', 'last_updated', 'temperature', 'humidity', 'pm10', 'pm25', 'pm100']
-    fields = ['name', 'purple_id', 'thingspeak_key', 'location', 'position']
-    # readonly_fields = ['location', 'position']
+class PurpleAirAdmin(MonitorAdmin):
+    # list_display = ['name', 'purple_id', 'last_updated', 'temperature', 'humidity', 'pm10', 'pm25', 'pm100']
+    list_display = MonitorAdmin.list_display[:]
+    list_display.insert(1, 'purple_id')
+
+    # fields = ['name', 'purple_id', 'thingspeak_key', 'location', 'position']
+    fields = MonitorAdmin.fields
+    fields.insert(1, 'purple_id')
+    fields.insert(2, 'thingspeak_key')
 
     def get_form(self, request, obj=None, **kwargs):
         if obj is None:
             kwargs['form'] = PurpleAirAddForm
         return super().get_form(request, obj, **kwargs)
-
-    def get_queryset(self, request):
-        return (super()
-            .get_queryset(request)
-            .select_related('latest')
-        )
 
     def render_change_form(self, request, context, *args, **kwargs):
         context.update({'export_form': DateRangeForm()})
@@ -32,42 +32,3 @@ class PurpleAirAdmin(admin.OSMGeoAdmin):
     def save_model(self, request, obj, *args, **kwargs):
         super().save_model(request, obj, *args, **kwargs)
         print(import_monitor_data(obj.pk, {'results': 1}))
-
-    def last_updated(self, instance):
-        if instance.latest:
-            return instance.latest.timestamp
-        return ''
-
-    def temperature(self, instance):
-        if instance.latest is None:
-            return ''
-
-        temps = []
-        if instance.latest.fahrenheit:
-            temps.append(f'{intcomma(floatformat(instance.latest.fahrenheit, 1))}°F')
-        if instance.latest.celcius:
-            temps.append(f'{intcomma(floatformat(instance.latest.celcius, 1))}°C')
-        return ' / '.join(temps)
-
-    def humidity(self, instance):
-        if instance.latest and instance.latest.humidity:
-            return f'{int(round(instance.latest.humidity))}%'
-        return ''
-
-    def pm10(self, instance):
-        if instance.latest:
-            return instance.latest.pm10_env or ''
-        return ''
-    pm10.short_description = 'PM1.0'
-
-    def pm25(self, instance):
-        if instance.latest:
-            return instance.latest.pm25_env or ''
-        return ''
-    pm25.short_description = 'PM2.5'
-
-    def pm100(self, instance):
-        if instance.latest:
-            return instance.latest.pm100_env or ''
-        return ''
-    pm100.short_description = 'PM10.0'
