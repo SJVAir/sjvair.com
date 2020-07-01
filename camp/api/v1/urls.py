2ed6f053ee36cf94df207c500510dedba9ebcc6c
dmpayton <derek.payton@gmail.com>
2020-06-30 20:55:18 -0700

    Initial BAM1022 support.

--- camp/api/v1/urls.py
+++ camp/api/v1/urls.py
@@ -7,6 +7,7 @@ app_name = 'api'
 urlpatterns = [
     path('time/', endpoints.CurrentTime.as_view(), name='current-time'),
     path('monitors/', include('camp.api.v1.monitors.urls', namespace='monitors')),
+    path('bam1022/', include('camp.api.v1.bam1022.urls', namespace='bam1022')),
     path('marker/<str:color>.png', endpoints.MapMarker.as_view(), name='marker'),
 
     # Deprecated.
