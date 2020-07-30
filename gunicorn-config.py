060b54c2a6b5b9aebe18bf2a8c5ff43384b08cee
dmpayton <derek.payton@gmail.com>
2020-07-30 11:01:47 -0700

    Gunicorn config.

--- gunicorn-config.py
+++ gunicorn-config.py
@@ -9,7 +9,6 @@ def env(key, default=None):
 bind = f"0:{env('PORT', '8000')}"
 
 max_requests = env("GUNICORN_MAX_REQUESTS", 1000)
-max_requests_jitter = env("GUNICORN_MAX_REQUESTS_JITTER", 25)
+max_requests_jitter = env("GUNICORN_MAX_REQUESTS_JITTER", 100)
 
-# worker_class = "uvicorn.workers.UvicornWorker"
-workers = env("GUNICORN_WORKERS", (multiprocessing.cpu_count() * 2) + 1)
+workers = env("GUNICORN_WORKERS", multiprocessing.cpu_count() * 2)
