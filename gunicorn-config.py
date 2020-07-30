e9278d4bbf81c5bc3baee7de6e03aef6ed375507
dmpayton <derek.payton@gmail.com>
2020-07-30 11:54:19 -0700

    Preload the app before workers are forked.

--- gunicorn-config.py
+++ gunicorn-config.py
@@ -8,7 +8,7 @@ def env(key, default=None):
 
 bind = f"0:{env('PORT', '8000')}"
 
-preload = True
+preload_app = True
 
 max_requests = env("GUNICORN_MAX_REQUESTS", 1000)
 max_requests_jitter = env("GUNICORN_MAX_REQUESTS_JITTER", 100)
