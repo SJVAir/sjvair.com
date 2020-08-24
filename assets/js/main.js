30095d2c94c43eb20bbc3b81af476de161bdc5c1
dmpayton <derek.payton@gmail.com>
2020-08-24 12:58:13 -0700

    Add modals for about page images.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  function getAll(selector) {
+    var parent = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+
+    return Array.prototype.slice.call(parent.querySelectorAll(selector), 0);
+  }
+
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = getAll('.navbar-burger');
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
@@ -16,6 +22,43 @@ document.addEventListener('DOMContentLoaded', () => {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
+      });
+    });
+  }
+
+  // Modals
+  function openModal(target) {
+    var $target = document.getElementById(target);
+    rootEl.classList.add('is-clipped');
+    $target.classList.add('is-active');
+  }
+
+  function closeModals() {
+    rootEl.classList.remove('is-clipped');
+    $modals.forEach(function ($el) {
+      $el.classList.remove('is-active');
+    });
+  }
+
+  var rootEl = document.documentElement;
+  var $modals = getAll('.modal');
+  var $modalButtons = getAll('.modal-button');
+  var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+
+  if ($modalButtons.length > 0) {
+    $modalButtons.forEach(function ($el) {
+      $el.addEventListener('click', function (event) {
+        event.preventDefault();
+        var target = $el.dataset.target;
+        openModal(target);
+      });
+    });
+  }
+
+  if ($modalCloses.length > 0) {
+    $modalCloses.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        closeModals();
       });
     });
   }
