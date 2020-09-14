53c389c5d510050719e0de2babf339c76bc25a6e
dmpayton <derek.payton@gmail.com>
2020-09-14 10:06:39 -0700

    Update MonitorAccessMiddleware to account for non-monitor endpoints.

--- camp/api/middleware.py
+++ camp/api/middleware.py
@@ -9,8 +9,17 @@ class MonitorAccessMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def __call__(self, request):
+        return self.get_response(request)
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         request.monitor = None
+
+        # We only care about the `monitors` endpoints for now, this should be
+        # refactored when `sensors` goes away. Also: account for API versions.
+        if not request.path.startswith('/api/1.0/monitors/'):
+            return None
+
         if 'monitor_id' in view_kwargs:
             monitor_id = view_kwargs['monitor_id']
             try:
@@ -29,7 +38,11 @@ class MonitorAccessMiddleware:
     def authorization_required(self, request):
         if request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
             return True
-        return request.monitor.is_hidden
+
+        if request.monitor is not None:
+            return request.monitor.is_hidden
+
+        return False
 
     def get_access_key(self, request):
         access_key = request.GET.get('access_key')
