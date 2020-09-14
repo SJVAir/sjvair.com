be05bf58d95a19f2932b26784ccd201ea0bb3827
dmpayton <derek.payton@gmail.com>
2020-09-14 10:09:25 -0700

    Fix context processor for Django 3.x changes.

--- camp/context_processors.py
+++ camp/context_processors.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.views.debug import get_safe_settings
+from django.views.debug import SafeExceptionReporterFilter
 
 
 def settings_context(request):
-    safe_settings = get_safe_settings()
+    safe_settings = SafeExceptionReporterFilter().get_safe_settings()
     safe_settings['GOOGLE_MAPS_API_KEY'] = settings.GOOGLE_MAPS_API_KEY
     return {'settings': safe_settings}
