9e3ceb52bca1502294e98658b80034330bc99d3b
dmpayton <derek.payton@gmail.com>
2020-09-14 23:28:10 -0700

    Adjust map marker text/background colors.

---
+++ assets/map/utils/colors.js
@@ -0,0 +1,11 @@
+export default {
+    white: 'ffffff',
+    gray: '969696',
+    black: '000000',
+    green: '00e400',
+    yellow: 'ffff00',
+    orange: 'ff7e00',
+    red: 'ff0000',
+    purple: '8f3f97',
+    maroon: '7e0023'
+}
