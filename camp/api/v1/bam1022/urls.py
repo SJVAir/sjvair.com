718d81493e082979f1de1618c966fd593dac452d
dmpayton <derek.payton@gmail.com>
2020-12-28 13:46:54 -0800

    Remove legacy bam1022 api that was never actually a thing.

--- camp/api/v1/bam1022/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-
-from . import endpoints
-
-app_name = 'bam1022'
-
-urlpatterns = [
-    path('<monitor_id>/entries/', endpoints.EntryList.as_view(), name='entry-list'),
-]
