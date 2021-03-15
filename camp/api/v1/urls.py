605524932207bb02513b210efa71d58666ec4c06
dmpayton <derek.payton@gmail.com>
2021-03-14 23:39:53 -0700

    Adjustments to methane data endpoints.

--- camp/api/v1/urls.py
+++ camp/api/v1/urls.py
@@ -1,14 +1,15 @@
 from django.urls import include, path
 
 from . import endpoints
-from .monitors.endpoints import MethaneData
+from .monitors.endpoints import MethaneDataUpload
 
 app_name = 'api'
 
 urlpatterns = [
     path('time/', endpoints.CurrentTime.as_view(), name='current-time'),
     path('monitors/', include('camp.api.v1.monitors.urls', namespace='monitors')),
-    path('methane-data/', MethaneData.as_view(), name='methane-data'),
+    path('methane/<int:methane_id>/upload/', MethaneDataUpload.as_view(), name='methane-data-upload'),
+    # path('methane/<int:methane_id>/data/', MethaneData.as_view(), name='methane-data'),
     path('marker.png', endpoints.MapMarker.as_view(), name='marker'),
 
     # Deprecated.
