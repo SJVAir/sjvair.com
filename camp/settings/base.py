becc13b35ead2c476980ed6d1d4d0770607853e8
dmpayton <derek.payton@gmail.com>
2021-07-26 17:39:41 -0700

    Update account templates.

--- camp/settings/base.py
+++ camp/settings/base.py
@@ -143,6 +143,12 @@ AUTHENTICATION_BACKENDS = (
 
 AUTH_USER_MODEL = 'accounts.User'
 
+LOGIN_URL = 'account:login'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
