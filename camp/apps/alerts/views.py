b054b24826bceb142a18c9d0ce50a3dd4c49a50b
dmpayton <derek.payton@gmail.com>
2021-08-17 01:17:59 -0700

    Split alerts and subscriptions into separate accounts pages.

--- camp/apps/alerts/views.py
+++ camp/apps/alerts/views.py
@@ -6,35 +6,32 @@ from camp.apps.alerts.models import Alert, Subscription
 from camp.apps.monitors.models import Monitor
 
 
-class SubscriptionList(LoginRequiredMixin, vanilla.TemplateView):
+class AlertList(LoginRequiredMixin, vanilla.ListView):
+    model = Alert
+    template_name = 'account/alerts.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = (queryset
+            .filter(
+                monitor__subscriptions__user=self.request.user,
+                end_time__isnull=True
+            )
+            .select_related('monitor')
+            .distinct()
+        )
+        return queryset
+
+
+class SubscriptionList(LoginRequiredMixin, vanilla.ListView):
+    model = Subscription
     template_name = 'account/subscriptions.html'
 
     def get_queryset(self):
-        return (Subscription.objects
+        queryset = super().get_queryset()
+        queryset = (queryset
             .filter(user=self.request.user)
+            .select_related('monitor')
+            .distinct()
         )
-
-    def get_subscription_list(self):
-        queryset = self.get_queryset()
-        monitor_ids = queryset.values_list('monitor_id', flat=True)
-        monitors = {m.pk: m for m in Monitor.objects.filter(pk__in=monitor_ids)}
-
-        subscription_list = []
-        for subscription in queryset:
-            subscription.monitor = monitors[subscription.monitor_id]
-            subscription_list.append(subscription)
-
-        return subscription_list
-
-    def get_current_alerts(self):
-        alerts = Alert.objects.filter(
-            monitor__subscriptions__user=self.request.user,
-            end_time__isnull=True
-        ).select_related('monitor').distinct()
-        return alerts
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['alert_list'] = self.get_current_alerts()
-        context['subscription_list'] = self.get_subscription_list()
-        return context
+        return queryset
