0ac18d644b5079e46e90ab7f4b7e3f7748c5567a
dmpayton <derek.payton@gmail.com>
2022-01-25 21:18:18 -0800

    Add script to export analysis of PurpleAir A and B sensors.

---
+++ camp/apps/monitors/linreg.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+from django.db.models import F
+from sklearn.linear_model import LinearRegression
+
+
+
+def linear_regression(endog_qs, exog_qs, exog_coefs):
+    # endog is generally the reference data that we want to fit to
+    endog_qs = (endog_qs
+        .annotate(endog_pm25_env=F('pm25_env'))
+        .values('timestamp', 'endog_pm25_env')
+    )
+
+    if not endog_qs.exists():
+        return
+
+    endog_df = pd.DataFrame(endog_qs).set_index('timestamp')
+    endog_df = pd.to_numeric(endog_df.endog_pm25_env)
+    endog_df = endog_df.resample('H').mean()
+
+    # exog is the data we want to try to calibrate to the endog
+    exog_qs = exog_qs.values('timestamp', *exog_coefs)
+
+    if not exog_qs.exists():
+        return
+
+    exog_df = pd.DataFrame(exog_qs).set_index('timestamp')
+    exog_df[exog_df.columns] = exog_df[exog_df.columns].apply(pd.to_numeric, errors='coerce')
+    exog_df = exog_df.resample('H').mean()
+
+    # Merge the dataframes
+    merged = pd.concat([endog_df, exog_df], axis=1, join="inner")
+    merged = merged.dropna()
+
+    if not len(merged):
+        return
+
+    endog = merged['endog_pm25_env']
+    exog = merged[exog_coefs]
+
+    try:
+        reg = LinearRegression()
+        reg.fit(exog, endog)
+    except ValueError as err:
+        # import code
+        # code.interact(local=locals())
+        print('Linear Regression Error:', err)
+        return
+
+    return {
+        'reg': reg,
+        'df': merged,
+        'r2': reg.score(exog, endog),
+        'intercept': reg.intercept_,
+        'coefs': dict(zip(exog_coefs, reg.coef_)),
+    }
