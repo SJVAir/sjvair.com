d8c13aea43e2a851f21c2f5986744b21c063fdb0
Alexander McCormick <alexandermccormick@protonmail.com>
2022-02-15 15:02:47 -0800

    refactored using Vue3 and TypeScript

---
+++ assets/vue/main.ts
@@ -0,0 +1,28 @@
+import { createApp, reactive} from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import MonitorDetail from "./components/MonitorDetail";
+import { D3Service, MonitorsService } from "./services";
+
+const monitorService = reactive(new MonitorsService());
+const d3Service = reactive(new D3Service());
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/monitor/:id',
+      name: 'details',
+      component: MonitorDetail,
+      props: true,
+      alias: '/monitor/:id/?timestamp__gte=:timestamp__gte&timestamp__lte=:timestamp__lte'
+    }
+  ]
+});
+
+const app = createApp(App);
+
+app.use(router);
+app.provide<typeof d3Service>("D3Service", d3Service);
+app.provide<typeof monitorService>("MonitorsService", monitorService);
+app.mount("#app");
