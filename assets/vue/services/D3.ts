bc43c1d5efb51f2838bb4e2a0d0b981512dbca24
Alexander McCormick <alexandermccormick@protonmail.com>
2022-02-15 15:47:11 -0800

    creates proper background service for d3

---
+++ assets/vue/services/D3.ts
@@ -0,0 +1,30 @@
+import BackgroundServiceClient from "../webworkers/BackgroundServiceClient";
+import D3BackgroundService from "./D3Background?worker";
+import type { ID3Service } from "../types";
+
+export class D3Service extends BackgroundServiceClient<ID3Service> {
+  static instance: D3Service;
+
+  constructor() {
+    if (D3Service.instance) {
+      return D3Service.instance;
+    }
+
+    super(new D3BackgroundService());
+
+    D3Service.instance = this;
+  }
+
+  async updateLastActiveLimit(limit: any) {
+    return await this.run("updateLastActiveLimit", limit);
+  }
+
+  async computeSegments(data: Array<any>, isNext?: (prev: any, curr: any) => boolean): Promise<{ gaps: Array<any>, segments: any }> {
+    return await this.run("computeSegments", data, isNext);
+  }
+  
+  async lineDefined(dataPoint: any) {
+    return await this.run("lineDefined", dataPoint);
+  }
+  
+}
---
+++ assets/vue/services/index.ts
@@ -0,0 +1,4 @@
+export * from "./Monitors";
+export * from "./MonitorsBackground";
+export * from "./D3";
+export * from "./D3Background";
