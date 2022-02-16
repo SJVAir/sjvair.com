42fb286c1a31439579d53f01d3d37fd97fe2c3af
Alexander McCormick <alexandermccormick@protonmail.com>
2022-02-15 16:01:08 -0800

    converts vue components to Vue 3 and typescript

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: resolve("./assets/vue/"),
+  plugins: [vue()],
+  resolve: {
+    extensions: [".js", ".ts", ".json"],
+  },
+  build: {
+    outDir: resolve("./dist/vue"),
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: resolve("./assets/vue/main.ts"),
+      },
+    },
+  },
+})
