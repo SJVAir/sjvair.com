5357fcb8587219909f33523815ccd4a56e7e3d08
Alexander McCormick <alexandermccormick@protonmail.com>
2022-02-16 16:23:24 -0800

    adds django-vite for HMR support

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue"
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve("./assets/vue/"),
+  base: "/static/",
   plugins: [vue()],
   resolve: {
     extensions: [".js", ".ts", ".json"],
