42d7345b45e7446e06dd8ff395f721b8de381a0b
Alexander McCormick <alexandermccormick@protonmail.com>
2022-04-18 17:31:19 -0700

    extracts MonitorVisibility from Monitor model to its own service

---
+++ assets/vue/services/MonitorVisibility.ts
@@ -0,0 +1,38 @@
+import type { Monitor } from "./Monitor";
+import type { IMonitorVisibility } from "../types";
+
+export class MonitorVisibility implements IMonitorVisibility {
+  SJVAirPurple: boolean = true;
+  SJVAirInactive: boolean = false; // Currently unused in project, do we need?
+  SJVAirBAM: boolean = true;
+  PurpleAir: boolean = true;
+  PurpleAirInside: boolean = false;
+  AirNow: boolean = true;
+  displayInactive: boolean = false;
+
+  isVisible(m: Monitor): boolean {
+    // showSJVAirPurple
+    // showSJVAirInactive
+    // showSJVAirBAM
+    // showPurpleAir
+    // showPurpleAirInside
+    // showAirNow
+
+    if(!this.displayInactive && !m.data.is_active){
+      return false;
+    }
+
+    if(m.data.device == 'PurpleAir') {
+      return m.data.is_sjvair 
+        ? this.SJVAirPurple
+        : this.PurpleAir && (this.PurpleAirInside || m.data.location == 'outside');
+
+    } else if (m.data.device == 'BAM1022'){
+      return this.SJVAirBAM;
+
+    }  else if (m.data.device == 'AirNow'){
+      return this.AirNow;
+    }
+    return false;
+  }
+}
