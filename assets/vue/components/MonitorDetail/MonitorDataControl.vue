7963d5e2dfa1428be0e6c476de106ac48fdabb4d
Alexander McCormick <alexandermccormick@protonmail.com>
2022-04-21 14:25:51 -0700

    bind datepicker value to monitorService.dateRange

--- assets/vue/components/MonitorDetail/MonitorDataControl.vue
+++ assets/vue/components/MonitorDetail/MonitorDataControl.vue
@@ -1,46 +1,14 @@
 <script setup lang="ts">
-import Datepicker from "vue3-date-time-picker";
-import "vue3-date-time-picker/dist/main.css"
+import Datepicker from "@vuepic/vue-datepicker";
+import '@vuepic/vue-datepicker/dist/main.css';
 import MonitorSubscription from "../MonitorSubscription.vue";
 
-import { inject, ref, watch } from "vue";
-import { useRouter } from "vue-router";
+import { inject } from "vue";
 import type { MonitorsService } from "../../services";
 
 const monitorsService = inject<MonitorsService>("MonitorsService")!;
-const dateRange = ref([monitorsService.dateRange.start.toDate(), monitorsService.dateRange.end.toDate()]);
 const emit = defineEmits<{(e: "loadAll"): void}>();
-const router = useRouter();
 
-
-function updateURL() {
-  router.push({
-    name: "details",
-    params: {
-      id: monitorsService.activeMonitor!.data.id
-    },
-    query: {
-      timestamp__gte: monitorsService.dateRange.start.toISOString(),
-      timestamp__lte: monitorsService.dateRange.end.toISOString()
-    }
-  });
-};
-
-//function checkStartDate(date: Date) {
-//  // Date must be lte endDate and lte today.
-//  // true means disabled.
-//  const gteEnd = date > MonitorsService.dateRange.end.toDate();
-//  const gteTonight = date > dateUtil.dayjs().endOf('day').toDate();
-//  return gteEnd || gteTonight;
-//}
-//
-//function checkEndDate(date) {
-//  // Date must be gte startDate and lte today.
-//  // true means disabled.
-//  const lteStart = date < dateUtil(startDate).startOf('day').toDate();
-//  const gteTonight = date > dateUtil().endOf('day').toDate();
-//  return lteStart || gteTonight;
-//}
 
 function downloadCSV() {
   monitorsService.downloadCSV();
@@ -50,11 +18,6 @@ function loadAllEntries() {
   emit("loadAll");
 }
 
-watch(
-  () => dateRange,
-  () => updateURL()
-);
-
 </script>
 
 <template>
@@ -63,7 +26,7 @@ watch(
       <div class="field">
         <label for="startDate" class="label is-small has-text-weight-normal">Date Range</label>
         <div class="control">
-          <Datepicker v-model="dateRange" range />
+          <Datepicker v-model="monitorsService.dateRange" range />
         </div>
       </div>
     </div>
