5ba3fa1e73584beff82cba9c0c48e32f90d94e60
Alexander McCormick <alexandermccormick@protonmail.com>
2022-04-21 14:27:17 -0700

    convert to array subclass

--- assets/vue/models/DateRange.ts
+++ assets/vue/models/DateRange.ts
@@ -1,14 +1,30 @@
 import { dateUtil } from "../utils";
-import { Dayjs } from "dayjs";
 
-export class DateRange {
-  start: Dayjs = dateUtil.dayjs().subtract(3, "day").startOf("day");
-  end: Dayjs = dateUtil.dayjs().endOf("day");
+export class DateRange extends Array<string> {
 
   constructor(range?: DateRange) {
+    let start: string = dateUtil.dayjs().subtract(3, "day").startOf("day").toISOString();
+    let end: string = dateUtil.dayjs().endOf("day").toISOString();
     if (range) {
-      this.start = dateUtil.dayjs(range.start).startOf("day");
-      this.end = dateUtil.dayjs(range.end).endOf("day");
+      start = dateUtil.dayjs(range.start).startOf("day").toISOString();
+      end = dateUtil.dayjs(range.end).endOf("day").toISOString();
     }
+    super(start, end);
+  }
+
+  get start() {
+    return this[0];
+  }
+
+  set start(date: string) {
+    this.splice(0, 1, dateUtil.dayjs(date).startOf("day").toISOString());
+  }
+
+  get end() {
+    return this[1];
+  }
+
+  set end(date: string) {
+    this.splice(1, 1, dateUtil.dayjs(date).endOf("day").toISOString());
   }
 }
