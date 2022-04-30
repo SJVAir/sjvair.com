a362e6eb47fa01ec378e5dc266677d4f8eed6fb4
Alexander McCormick <alexandermccormick@protonmail.com>
2022-04-29 17:41:58 -0700

    simplifies marker generation

--- assets/vue/utils/map.ts
+++ assets/vue/utils/map.ts
@@ -1,45 +1,23 @@
-import { readableColor } from "color2k";
+import { readableColor, darken } from "color2k";
 import { Point } from "leaflet";
 import { Monitor } from "../models";
 import { dateUtil, pmValueToColor } from ".";
 
 import type { Marker } from "leaflet";
 
-function getMarkerShape(m: Monitor) {
-  switch(m.data.device) {
-    case "AirNow": 
-      return "triangle";
-    case "BAM1022":
-      return "triangle";
-    case "PurpleAir":
-      return (m.data.is_sjvair) ? "circle" : "square";
-    default:
-      console.error(`Unknown device type for monitor ${ m.data.id }`);
-      return "diamond";
-  }
-}
-
 export function genMarker(m: Monitor): Marker | undefined {
-
-  // No marker needed if there's no latest data
-  //if (!m.data.latest) {
-  //  return;
-  //}
-
-  const shape = getMarkerShape(m);
-  const fillColor = pmValueToColor(+m.data.latest[m.displayField]);
-  const textColor = readableColor(fillColor);
   // @ts-ignore: Property 'shapeMarker' does not exist on type Leaflet
   const marker = L.shapeMarker(m.data.position.coordinates.reverse(), {
-    color: "#000000", // background color
-    fillColor,
+    color: m.markerParams.border_color,
+    weight: m.markerParams.border_size,
+    fillColor: m.markerParams.fill_color,
     fillOpacity: 1,
-    radius: 12,
-    shape
+    radius: m.markerParams.size,
+    shape: m.markerParams.shape
   })
 
   marker.bindTooltip(`
-    <div class="monitor-tooltip-container" style="background-color: ${ fillColor }; color: ${ textColor }">
+    <div class="monitor-tooltip-container" style="background-color: ${ m.markerParams.fill_color }; color: ${ readableColor(m.markerParams.fill_color!) }">
       <p class="monitor-tooltip-date">${ dateUtil.$prettyPrint(m.data.latest.timestamp) }</p>
       <p class="is-size-4 is-underlined">${ m.data.name }</p>
       <div class="mt-1 is-flex is-justify-content-space-between is-align-items-flex-start is-flex-wrap-nowrap">
