17058b07689fc3a9c172574c4bbd46462ae2f9e4
Alexander McCormick <alexandermccormick@protonmail.com>
2022-05-02 20:37:55 -0700

    Moves MonitorField assignments to Monitor constructor

--- assets/vue/utils/constants.ts
+++ assets/vue/utils/constants.ts
@@ -1,38 +1,5 @@
-import { MonitorField } from "../models/MonitorField";
-
 export const MonitorFieldColors = {
   pm25: "#00ccff",
   pm25_avg_15: "#006699",
   pm25_avg_60: "#000033"
-} as const;
-
-export const MonitorFields = MonitorField.genMulti(
-  ["pm10", "PM 1.0", ""],
-  ["pm25", "PM 2.5", "2m"],
-  ["pm25_avg_15", "PM 2.5", "15m"],
-  ["pm25_avg_60", "PM 2.5", "60m"],
-  ["pm100", "PM 10", ""]
-)
-
-export const MonitorTypesMeta = {
-  AirNow: {
-    monitorFields: MonitorField.genMulti(
-      ["pm25", "PM 2.5", "60m"],
-      ["pm100", "PM 10", ""]
-    )
-  },
-  BAM1022: {
-    monitorFields: MonitorField.genMulti(
-      ["pm25", "PM 2.5", "60m"]
-    )
-  },
-  PurpleAir: {
-    monitorFields: MonitorField.genMulti(
-      ["pm10", "PM 1.0", ""],
-      ["pm25", "PM 2.5", "2m"],
-      ["pm25_avg_15", "PM 2.5", "15m"],
-      ["pm25_avg_60", "PM 2.5", "60m"],
-      ["pm100", "PM 10", ""]
-    )
-  }
 } as const;
