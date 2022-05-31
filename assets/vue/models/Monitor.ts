10c80343b3d35f4589a1ee8e694c0549a974740f
Alexander McCormick <alexandermccormick@protonmail.com>
2022-05-30 20:05:27 -0700

    adds lastUpdated property and marker tooltip color assignments

--- assets/vue/models/Monitor.ts
+++ assets/vue/models/Monitor.ts
@@ -1,6 +1,6 @@
 import { darken, toHex } from "color2k";
 import { MonitorField } from "./MonitorField";
-import { Colors, pmValueToColor } from "../utils";
+import { Colors, dateUtil, valueToColor } from "../utils";
 import type { ChartDataField, IMarkerParams, IMonitor, IMonitorData, MonitorDataField } from "../types";
 
 export const Display_Field = "pm25_avg_15" as const;
@@ -11,6 +11,7 @@ function getMarkerParams(monitorData: IMonitorData): IMarkerParams {
     border_color: toHex(darken(fill_color, .1)),
     border_size: 1,
     fill_color,
+    value_color: fill_color,
     size: 8, //monitorData.is_active ? 16 : 6,
     shape: 'square'
   }
@@ -28,27 +29,32 @@ function getMarkerParams(monitorData: IMonitorData): IMarkerParams {
       params.shape = "diamond";
   }
 
-  if (monitorData.is_active && monitorData.latest) {
-    params.fill_color = pmValueToColor(+monitorData.latest[Monitor.displayField]);
-    params.border_color = toHex(darken(params.fill_color, .1));
+  if (monitorData.latest) {
+    const valueColor = valueToColor(+monitorData.latest[Monitor.displayField], MonitorField.levels);
+    params.value_color = valueColor;
 
-    switch(monitorData.device) {
-      case "AirNow": 
-      case "BAM1022":
-        params.size = 14;
-        break;
-      case "PurpleAir":
-        if (monitorData.is_sjvair) {
-          params.size = 16
-        } else {
-          params.size = 10;
-        }
-        break;
-    }
+    if (monitorData.is_active) {
+      params.fill_color = valueToColor(+monitorData.latest[Monitor.displayField], MonitorField.levels);
+      params.border_color = toHex(darken(params.fill_color, .1));
 
-    if (monitorData.location === "inside"){
-      params.border_color = `#${ Colors.black }`;
-      params.border_size = 2;
+      switch(monitorData.device) {
+        case "AirNow": 
+        case "BAM1022":
+          params.size = 14;
+          break;
+        case "PurpleAir":
+          if (monitorData.is_sjvair) {
+            params.size = 16
+          } else {
+            params.size = 10;
+          }
+          break;
+      }
+
+      if (monitorData.location === "inside"){
+        params.border_color = `#${ Colors.black }`;
+        params.border_size = 2;
+      }
     }
   }
 
@@ -63,6 +69,7 @@ export class Monitor implements IMonitor {
   data: IMonitorData;
   dataFields: Array<ChartDataField>;
   displayField: string = Monitor.displayField;
+  lastUpdated: string;
   markerParams: IMarkerParams;
   monitorFields!: Record<MonitorDataField, MonitorField>;
 
@@ -71,7 +78,7 @@ export class Monitor implements IMonitor {
       case "AirNow":
         this.monitorFields = MonitorField.genMulti(
           ["pm25", "PM 2.5", "60m", monitorData],
-          ["pm100", "PM 10", "", monitorData]
+          ["pm100", "PM 10", "60M", monitorData]
         );
         break;
 
@@ -83,7 +90,7 @@ export class Monitor implements IMonitor {
 
       case "PurpleAir":
         this.monitorFields = MonitorField.genMulti(
-          ["pm10", "PM 1.0", "", monitorData],
+          ["pm10", "PM 1.0", "2m", monitorData],
           ["pm25", "PM 2.5", "2m", monitorData],
           ["pm25_avg_15", "PM 2.5", "15m", monitorData],
           ["pm25_avg_60", "PM 2.5", "60m", monitorData],
@@ -95,6 +102,10 @@ export class Monitor implements IMonitor {
     this.data = monitorData;
     this.dataFields = Object.keys(this.monitorFields) as Array<ChartDataField>;
     this.markerParams = getMarkerParams(monitorData);
+
+    this.lastUpdated = (monitorData.latest && Object.keys(monitorData.latest).length > 1)
+      ? dateUtil.dayjs(monitorData.latest.timestamp).fromNow()
+      : "never";
   }
 
 }
