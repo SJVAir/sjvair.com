8bbe882168139cc7f4c6a4b418aea4634e6b981f
Alexander McCormick <alexandermccormick@protonmail.com>
2022-05-30 20:08:24 -0700

    converts valueToColor to be more generic

--- assets/vue/utils/index.ts
+++ assets/vue/utils/index.ts
@@ -1,33 +1,32 @@
 import { mix } from "color2k";
 import { MonitorField } from "../models";
+import {IColorMap} from "../types";
 import { dateUtil } from "./date";
 import { http } from "./http";
 
-
 export * from "./colors";
 export * from "./constants";
-export * from "./map";
 export {
   dateUtil,
   http
 }
 
-export function pmValueToColor(value: number) {
-  const lastLvl = MonitorField.levels[MonitorField.levels.length - 1];
+export function valueToColor(value: number, colors: Array<IColorMap>) {
+  const lastLvl = colors[colors.length - 1];
 
   if (value >= lastLvl.min) {
     return `#${lastLvl.color}`;
 
   } else if (value <= 0) {
-    return `#${MonitorField.levels[0].color}`;
+    return `#${colors[0].color}`;
 
   } else {
-    for (let i = 0; i <= MonitorField.levels.length - 1; i++) {
-      if (MonitorField.levels[i].min > value) {
+    for (let i = 0; i <= colors.length - 1; i++) {
+      if (colors[i].min > value) {
         // Level for current value
-        const min = MonitorField.levels[i-1];
+        const min = colors[i-1];
         // Level threshold for current value
-        const max = MonitorField.levels[i]
+        const max = colors[i];
         // Difference between max and min values => total steps in level
         const lvlDiff = min.min === -Infinity ? max.min : max.min - min.min;
         // Difference between threshold and current values => steps remaining for current level
@@ -38,7 +37,8 @@ export function pmValueToColor(value: number) {
         const diff = divisable / lvlDiff;
 
         // Color magic
-        return mix(`#${min.color}`, `#${max.color}`, diff);
+        const newColor = mix(`#${min.color}`, `#${max.color}`, diff);
+        return newColor;
       }
     }
 
