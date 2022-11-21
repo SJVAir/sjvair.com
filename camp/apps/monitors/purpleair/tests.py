ed4824a47185c1b05a59a418c8c76611fdf2e069
dmpayton <derek.payton@gmail.com>
2022-11-21 04:41:54 -0800

    Get tests passing again.

--- camp/apps/monitors/purpleair/tests.py
+++ camp/apps/monitors/purpleair/tests.py
@@ -8,18 +8,13 @@ from .models import PurpleAir
 class PurpleAirTests(TestCase):
     fixtures = ['purple-air.yaml']
     def setUp(self):
-        self.monitor = PurpleAir.objects.get(data__sensor_index=8892)
+        self.monitor = PurpleAir.objects.get(purple_id=8892)
 
     def test_create_entry(self):
         payload = purpleair_api.get_sensor(self.monitor.purple_id)
         (a, b) = self.monitor.create_entries(payload)
-        self.monitor.process_entry(a)
-        self.monitor.process_entry(b)
         self.monitor.check_latest(a)
         self.monitor.check_latest(b)
-
-        import code
-        code.interact(local=locals())
 
         assert a.sensor == 'a'
         assert a.position == self.monitor.position
