00b7c0fb4cf341107eeb22b028c9e5d377cb5990
Alexander McCormick <a.mccormick989@gmail.com>
2022-12-12 12:49:07 -0800

    Feat: Closest Monitor Endpoint (#122)
    * initial closest-monitor endpoint
    
    * Update the losest monitor endpoint.
    
    * adds alternate version of ClosestMonitor enpoint for benchmarking
    
    * removes unused parameter
    
    * removes unused import
    
    * Changes to the closest monitor endpoint, per convo with @alexandermccormick
    
    * adds monitors/closest endpoint
    
    Co-authored-by: Alexander McCormick <alexandermccormick@protonmail.com>
    Co-authored-by: dmpayton <derek.payton@gmail.com>

--- camp/utils/forms.py
+++ camp/utils/forms.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 from django import forms
 from django.contrib.admin.widgets import AdminDateWidget
+from django.contrib.gis.geos import Point
 
 
 class DateRangeForm(forms.Form):
@@ -12,3 +13,14 @@ class DateRangeForm(forms.Form):
     #     # We want the date range to be inclusive for database
     #     # lookups, so add one day to the end date.
     #     return self.cleaned_data['end_date'] + timedelta(days=1)
+
+
+class LatLonForm(forms.Form):
+    latitude = forms.FloatField(min_value=-90, max_value=90, required=True)
+    longitude = forms.FloatField(min_value=-180, max_value=180, required=True)
+
+    @property
+    def point(self):
+        assert self.is_valid()
+        data = self.cleaned_data
+        return Point(data['longitude'], data['latitude'], srid=4326)
