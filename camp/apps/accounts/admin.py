494e37f926618700ab70f206fde3235f12034b8a
Alexander McCormick <alexandermccormick@protonmail.com>
2023-08-08 16:39:31 -0700

    renames ugettext_lazy to gettext_lazy

--- camp/apps/accounts/admin.py
+++ camp/apps/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from camp.apps.accounts.models import User
 from camp.apps.alerts.admin import SubscriptionInline
