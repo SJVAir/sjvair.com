1bf39474d228798cf9f7924bd07cd029c224a956
Alexander McCormick <alexandermccormick@protonmail.com>
2023-08-10 17:00:39 -0700

    creates huey task for analyzing multisensor monitors

---
+++ camp/apps/qaqc/tasks.py
@@ -0,0 +1,15 @@
+from huey import crontab
+from huey.contrib.djhuey import db_periodic_task
+
+from camp.apps.qaqc.sensorlinreg import SensorLinearRegression
+from camp.apps.monitors.models import Monitor
+
+
+@db_periodic_task(crontab(hour='8', minute='30'), priority=50)
+def ab_regression():
+    qs = Monitor.objects.get_active_multisensor()
+
+    for monitor in qs:
+        linreg = SensorLinearRegression(monitor)
+        report = linreg.generate_regression()
+        report.save()
