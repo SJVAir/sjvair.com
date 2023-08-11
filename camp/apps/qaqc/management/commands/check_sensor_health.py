e853e106eb5f342f80a14b7eca4351019ac8887e
Alexander McCormick <alexandermccormick@protonmail.com>
2023-08-10 17:01:08 -0700

    creates command to run multisensor analysis on demand

---
+++ camp/apps/qaqc/management/commands/check_sensor_health.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from camp.apps.monitors.models import Monitor
+from camp.apps.qaqc.sensorlinreg import SensorLinearRegression
+from camp.apps.qaqc.tasks import ab_regression
+
+
+class Command(BaseCommand):
+    help = 'Run linear regressions against sensors for multisensor monitors'
+
+    def handle(self, *args, **options):
+        ab_regression.call_local()
