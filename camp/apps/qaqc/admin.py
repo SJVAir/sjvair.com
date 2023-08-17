c774827c3777179c10b75815d1430cbdf839db88
Alexander McCormick <alexandermccormick@protonmail.com>
2023-08-16 17:17:00 -0700

    creates SensorAnalysisInline for django admin

--- camp/apps/qaqc/admin.py
+++ camp/apps/qaqc/admin.py
@@ -1,3 +1,4 @@
+import pytz
 from django.contrib import admin
 #from django.db.models import Prefetch
 from django.template import Context, Template
@@ -10,23 +11,32 @@ from django_admin_inline_paginator.admin import TabularInlinePaginated
 from camp.apps.monitors.models import Monitor 
 from .models import SensorAnalysis
 
-#class SensorAnalysisInline(TabularInlinePaginated):
-#    extra = 0
-#    model = Monitor
-#    per_page = 30
-#    fields = ('id', 'name', 'is_active')
-#    readonly_fields = ('id', 'name', 'is_active')
-#
-#    def has_add_permission(self, request, obj):
-#        return False
-#
-#    def has_change_permission(self, request, obj):
-#        return False
+class SensorAnalysisInline(TabularInlinePaginated):
+    extra = 0
+    model = SensorAnalysis
+    per_page = 7
+    fields = ('get_created', 'get_grade', 'r2', 'start_date', 'end_date')
+    readonly_fields = fields
+    ordering = ('-created',)
+
+    def get_created(self, instance):
+        return (instance.created.astimezone(pytz.timezone('America/Los_Angeles'))
+                .strftime('%b. %d, %Y, %-I:%M %P'))
+    get_created.short_description = "Created"
+
+    def get_grade(self, instance):
+        return instance.grade
+    get_grade.short_description = "Grade"
+
+    def has_add_permission(self, request, obj):
+        return False
+
+    def has_change_permission(self, request, obj):
+        return False
 
 
 @admin.register(SensorAnalysis)
 class SensorAnalysisAdmin(admin.ModelAdmin):
-    #inlines = (SensorAnalysisInline,)
     list_display = ('pk', 'get_reference', 'start_date', 'end_date', 'r2', 'coef', 'intercept')
     readonly_fields = ('pk', 'get_reference', 'start_date', 'end_date', 'r2', 'coef', 'intercept')
     search_fields = ['monitor__name']
