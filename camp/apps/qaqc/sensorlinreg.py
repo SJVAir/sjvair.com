be13898a83ba25f038f15a31d1e47e2140af2b04
Alexander McCormick <alexandermccormick@protonmail.com>
2023-09-25 14:14:00 -0700

    check variance and calculate overall grade when regessing

--- camp/apps/qaqc/sensorlinreg.py
+++ camp/apps/qaqc/sensorlinreg.py
@@ -10,7 +10,8 @@ from django.db.models import F
 from django.utils import timezone
 from django.utils.functional import cached_property
 
-from sklearn.linear_model import LinearRegression as skLinearRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import explained_variance_score
 
 from .models import SensorAnalysis
 
@@ -53,26 +54,32 @@ class SensorLinearRegression:
         if self.endog is None or self.exog is None:
             return None
 
+        r2_weight = 1
+        v_weight = 1
+
         try:
-            linreg = skLinearRegression()
+            linreg = LinearRegression()
             linreg.fit(self.exog, self.endog)
+            r2=linreg.score(self.exog, self.endog)
+
+            variance = explained_variance_score(
+                self.exog['pm25_reported'].values,
+                self.endog['pm25_reported'].values
+            )
         except ValueError as err:
             print('QAQC Linear Regression Error:', err)
             return None
 
-        results = SensorAnalysis(
+        return SensorAnalysis(
             monitor=self.monitor,
-            r2=linreg.score(self.exog, self.endog),
+            r2=r2,
+            variance=variance,
+            grade=((r2 * r2_weight) + (variance * v_weight)) / (r2_weight + v_weight),
             intercept=linreg.intercept_,
             coef=linreg.coef_[0][0],
             start_date=self.start_date,
             end_date=self.end_date,
         )
-
-        if results.r2 < 0.9:
-            print(f'{self.monitor.name} is possibly bad: {results.r2}')
-
-        return results
     
     def __get_sensor_group(self, sensor):
         groups = self.df.groupby('sensor')
