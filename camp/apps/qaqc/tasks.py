b4edc6a0092707dc1793a39b0c243e8b075bb0a2
Alexander McCormick <alexandermccormick@protonmail.com>
2023-09-27 12:48:58 -0700

    updates task to call new ABLinearRegression.analyze method

--- camp/apps/qaqc/tasks.py
+++ camp/apps/qaqc/tasks.py
@@ -1,7 +1,7 @@
 from django_huey import db_periodic_task
 from huey import crontab
 
-from camp.apps.qaqc.sensorlinreg import SensorLinearRegression
+from camp.apps.qaqc.ablinreg import ABLinearRegression
 from camp.apps.monitors.models import Monitor
 
 
@@ -10,8 +10,8 @@ def ab_regression():
     qs = Monitor.objects.get_active_multisensor()
 
     for monitor in qs:
-        linreg = SensorLinearRegression(monitor)
-        analysis = linreg.generate_regression()
+        linreg = ABLinearRegression(monitor)
+        analysis = linreg.analyze()
 
         if analysis is not None:
             analysis.save_as_current()
