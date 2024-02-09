29f81e60ea08035ca1e1f1c5810f03a934163a96
dmpayton <derek.payton@gmail.com>
2024-02-08 19:41:49 -0800

    Update the django admin for better display of user info.

--- camp/apps/accounts/admin.py
+++ camp/apps/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.template.loader import render_to_string
 from django.utils.translation import gettext_lazy as _
 
 from camp.apps.accounts.models import User
@@ -32,8 +33,15 @@ class UserAdmin(UserAdmin):
         }),
     )
     inlines = [SubscriptionInline]
-    list_display = ('full_name', 'phone', 'email', 'date_joined', 'is_staff', 'is_active')
+    list_display = ('full_name', 'get_phone', 'email', 'date_joined', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'language', 'groups')
     ordering = ('email',)
     readonly_fields = ('date_joined', 'last_login')
     search_fields = ('email', 'full_name')
+
+    def get_phone(self, instance):
+        return render_to_string('admin/accounts/user_phone.html', {
+            'user': instance,
+        })
+    get_phone.short_description = 'Phone number'
+    get_phone.admin_order_field = 'phone'
