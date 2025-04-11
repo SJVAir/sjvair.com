32aefd8a66417d580eaccabcc15010d8c7077b34
dmpayton <derek.payton@gmail.com>
2025-04-11 15:25:25 -0700

    Wire up AQview.ENTRY_CONFIG.

--- camp/apps/monitors/aqview/tasks.py
+++ camp/apps/monitors/aqview/tasks.py
@@ -1,11 +1,6 @@
-from datetime import datetime, timedelta
-
 import esri2gpd
-import pytz
 
-from django.conf import settings
 from django.contrib.gis.geos import Point
-from django.utils import timezone
 
 
 from django_huey import db_task, db_periodic_task
@@ -14,7 +9,7 @@ from huey import crontab
 from camp.apps.monitors.models import Entry
 from camp.apps.monitors.aqview.models import AQview
 from camp.utils.counties import County
-from camp.utils.datetime import make_aware
+
 
 AQVIEW_URL = "https://gis.carb.arb.ca.gov/hosting/rest/services/Hosted/AQview_revised_PROD_view/FeatureServer/0"
 
@@ -51,24 +46,19 @@ def process_aqview_data(payload):
             data_provider_url=payload.get('dplink', ''),
         )
 
-    # Calculate the timestamp and make it timezone aware.
-    payload['timestamp'] = make_aware(
-        datetime.fromtimestamp(payload['maptime'] / 1000) - timedelta(hours=payload['hourindex']),
-        pytz.timezone('America/Los_Angeles')
-    )
+    entry = monitor.create_entry(payload)
 
-    monitor.create_entries(payload)
+    if entry:
+        # Legacy
+        try:
+            entry = monitor.entries.get(timestamp=entry.timestamp)
+            entry = monitor.process_entry(entry, payload)
+            entry.save()
+            print('\t[AQview] Entry updated:', payload['timestamp'])
+        except Entry.DoesNotExist:
+            entry = monitor.create_entry_legacy(payload)
+            print('\t[AQview] Entry created:', payload['timestamp'])
 
-    # Legacy
-    try:
-        entry = monitor.entries.get(timestamp=payload['timestamp'])
-        entry = monitor.process_entry(entry, payload)
-        entry.save()
-        print('\t[AQview] Entry updated:', payload['timestamp'])
-    except Entry.DoesNotExist:
-        entry = monitor.create_entry_legacy(payload)
-        print('\t[AQview] Entry created:', payload['timestamp'])
-
-    monitor.check_latest(entry)
-    if monitor.latest_id == entry.pk:
-        monitor.save()
+        monitor.check_latest(entry)
+        if monitor.latest_id == entry.pk:
+            monitor.save()
