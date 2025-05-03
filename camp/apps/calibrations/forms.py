2d24c06053a8ef7975181e59cefb87ffb1fe4e8b
dmpayton <derek.payton@gmail.com>
2025-05-02 22:17:46 -0700

    Fix dynamic calibration choices in DefaultCalibration admin form

--- camp/apps/calibrations/forms.py
+++ camp/apps/calibrations/forms.py
@@ -14,28 +14,27 @@ class DefaultCalibrationForm(forms.ModelForm):
 
     class Meta:
         model = DefaultCalibration
-        fields = '__all__'
+        fields = ['monitor_type', 'entry_type', 'calibration']
+        widgets = {
+            'calibration': forms.Select()
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        if self.instance.monitor_model and self.instance.entry_model:
+            choices = self.get_calibration_choices(self.instance)
+            self.fields['calibration'].choices = choices
 
         if not self.instance._state.adding:
             self.fields['monitor_type'].disabled = True
             self.fields['entry_type'].disabled = True
 
-        monitor_model = self.instance.monitor_model
-        entry_model = self.instance.entry_model
-
-        if monitor_model and entry_model:
-            config = monitor_model.ENTRY_CONFIG.get(entry_model)
-            if config:
-                choices = [('', '-- none --')] + [
-                    (c.name, c.name)
-                    for c in config.get('calibrations', [])
-                ]
-                self.fields['calibration'] = forms.ChoiceField(
-                    choices=choices,
-                    required=False,
-                    label='Calibration',
-                    initial=self.instance.calibration,
-                )
+    @classmethod
+    def get_calibration_choices(cls, instance):
+        choices = [('', '-- none --')] + [
+            (str(proc.name), str(proc.name))
+            for proc in instance.allowed_processors
+        ]
+        print('\n.get_calibration_choices()', choices)
+        return choices
