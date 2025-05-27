be728dbb2e129ac0b69b03630c186e3a38c49ba4
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-05-27 19:04:42 -0400

    Bone Structure of hms smoke feat
    ADD: app to apps/monitors/hms_smoke, api endpoint for the app. model for smoke data before refactoring other code! First commit :D.

---
+++ camp/api/v1/hms_smoke/tests.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+# Create your tests here.
+#TODO Rewrite Testcases from APITestCase to TestCase (Test local then add to git)
---
+++ camp/apps/monitors/hms_smoke/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HmsSmokeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hms_smoke'
