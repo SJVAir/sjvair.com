bd32148a071dafb2956c1041a6e0d55c11a6f88c
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-06-04 10:52:07 -0400

    Fix: Small bug fixes/testing

--- camp/apps/monitors/hms_smoke/apps.py
+++ camp/apps/monitors/hms_smoke/apps.py
@@ -2,8 +2,5 @@ from django.apps import AppConfig
 
 
 class HmsSmokeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'camp.apps.monitors.hms_smoke'
     
-    def ready(self):
-        from . import tasks
