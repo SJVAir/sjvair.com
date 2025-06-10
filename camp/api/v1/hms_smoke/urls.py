bf743a2852609bc5dbdf78f0c013481184681263
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-06-10 13:17:38 -0400

    Format: small changes

--- camp/api/v1/hms_smoke/urls.py
+++ camp/api/v1/hms_smoke/urls.py
@@ -8,9 +8,9 @@ app_name = "hms_smoke"
 
 urlpatterns = [
     path("ongoing", OngoingSmokeView.as_view(), name="ongoing_smoke" ),
-    path("ongoing/density", OngoingSmokeDensityView.as_view(), name="ongoing_smoke_density"),
+    path("ongoing/density/", OngoingSmokeDensityView.as_view(), name="ongoing_smoke_density"),
     path("last", LatestObeservableSmokeView.as_view(), name = "last_observable_smoke"),
-    path("last/density", LatestObeservableSmokeDensityView.as_view(), name="last_observable_smoke_density"),
+    path("last/density/", LatestObeservableSmokeDensityView.as_view(), name="last_observable_smoke_density"),
     path("<str:pk>", SelectSmokeView.as_view(), name="smoke_by_id"),
     path("", SmokeByTimestamp.as_view(), name="all_by_timestamp"),
     #EXPECTS UTC TIME INPUT
