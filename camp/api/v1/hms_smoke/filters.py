29e893d49bf24d79a8956edde5301954227636fb
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-06-10 17:13:17 -0400

    Initial refactor pt2
    Endpoint/model/mixin/url/filter/mixin implementation or remodeling. Commented out old code to switch back easily.

---
+++ camp/api/v1/hms_smoke/filters.py
@@ -0,0 +1,27 @@
+import django_filters
+from resticus.filters import FilterSet
+from camp.apps.integrate.hms_smoke.models import Smoke
+
+
+class SmokeFilter(FilterSet):
+    
+    class Meta: 
+        model = Smoke
+        fields = {
+            'density': [
+                'iexact',
+            ],
+            'start':[
+                'exact',
+                'lt', 'lte',
+                'gt', 'gte',
+            ],
+            'end':[
+                'exact',
+                'lt', 'lte',
+                'gt', 'gte',
+            ],
+            'satellite': [
+                'iexact',
+            ],
+        }
--- camp/api/v1/hms_smoke/urls.py
+++ camp/api/v1/hms_smoke/urls.py
@@ -3,15 +3,20 @@ from .endpoints import *
 
 app_name = "hms_smoke"
 
-
 urlpatterns = [
-    path("ongoing", OngoingSmokeView.as_view(), name="ongoing_smoke" ),
-    path("ongoing/density/", OngoingSmokeDensityView.as_view(), name="ongoing_smoke_density"),
-    path("last", LatestObeservableSmokeView.as_view(), name = "last_observable_smoke"),
-    path("last/density/", LatestObeservableSmokeDensityView.as_view(), name="last_observable_smoke_density"),
-    path("<str:pk>", SelectSmokeView.as_view(), name="smoke_by_id"),
-    path("", SmokeByTimestamp.as_view(), name="all_by_timestamp"),
-    #EXPECTS UTC TIME INPUT
-    path("time/", StartEndFilter.as_view(), name="start_end_filter")
-
+    path('', )
+    
 ]
+
+
+# urlpatterns = [
+#     path("ongoing", OngoingSmokeView.as_view(), name="ongoing_smoke" ),
+#     path("ongoing/density/", OngoingSmokeDensityView.as_view(), name="ongoing_smoke_density"),
+#     path("last", LatestObeservableSmokeView.as_view(), name = "last_observable_smoke"),
+#     path("last/density/", LatestObeservableSmokeDensityView.as_view(), name="last_observable_smoke_density"),
+#     path("<str:pk>", SelectSmokeView.as_view(), name="smoke_by_id"),
+#     path("", SmokeByTimestamp.as_view(), name="all_by_timestamp"),
+#     #EXPECTS UTC TIME INPUT
+#     path("time/", StartEndFilter.as_view(), name="start_end_filter")
+
+# ]
