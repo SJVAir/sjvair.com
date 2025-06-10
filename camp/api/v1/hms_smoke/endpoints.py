03189185c541f6201796e1a8fc0fd9b0d5c3a788
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-06-10 11:07:42 -0400

    Format: attempted to reduce complexity
    created currentTime() helper, and reduced redundantly long variable names + attempted pep8 format changes

--- camp/api/v1/hms_smoke/endpoints.py
+++ camp/api/v1/hms_smoke/endpoints.py
@@ -9,6 +9,7 @@ from django.shortcuts import get_object_or_404
 from smalluuid import SmallUUID
 import os
 from django.http import JsonResponse
+from ....apps.integrate.hms_smoke.services.helpers import *
 
 env = os.environ.get
 query_hours = int(os.environ.get('query_hours', 3))
@@ -18,31 +19,35 @@ class OngoingSmokeView(generics.ListEndpoint):
     serializer_class = SmokeSerializer
     
     def get_queryset(self):
-        queryset = query_ongoing_smoke(query_hours).order_by('-end')
+        queryset = ongoing(query_hours).order_by('-end')
         return queryset
 
 class OngoingSmokeDensityView(generics.ListEndpoint):
     model = Smoke
     serializer_class = SmokeSerializer
+    
     def get_queryset(self):
         densities = self.request.GET.getlist('density')
-        queryset = query_ongoing_density_smoke(query_hours, densities).order_by('-end')
+        queryset = ongoing_density(query_hours, densities).order_by('-end')
         return queryset
     
     
 class LatestObeservableSmokeView(generics.ListEndpoint):
     model = Smoke
     serializer_class = SmokeSerializer
+    
     def get_queryset(self):
-            queryset = query_latest_smoke().order_by('-observation_time')
+            queryset = latest().order_by('-observation_time')
             return queryset
+
 
 class LatestObeservableSmokeDensityView(generics.ListEndpoint):
     model = Smoke
     serializer_class = SmokeSerializer
+    
     def get_queryset(self):
         densities = self.request.GET.getlist('density')
-        queryset = query_latest_smoke_density(densities).order_by('-observation_time')
+        queryset = latest_density(densities).order_by('-observation_time')
         return queryset
         
 
@@ -54,15 +59,14 @@ class SelectSmokeView(generics.Endpoint):
     def get(self, *args, **kwargs):
         try:
             uuid_str = kwargs['pk']
-            #Error check the uuid_str in the bar
-            SmallUUID(uuid_str)
-            strCheck(uuid_str)
+            SmallUUID(strCheck(uuid_str))
             smoke = get_object_or_404(Smoke, pk=uuid_str)
             smoke_serialized = SmokeSerializer(smoke).serialize()
             return JsonResponse({"data": smoke_serialized})
         except Exception as e:
             uuid_str = self.kwargs.get('pk')
             raise Http404(f"There was a problem retrieving smoke data for id = {uuid_str}")
+    
     
 class SmokeByTimestamp(generics.ListEndpoint):
     model = Smoke
@@ -73,8 +77,6 @@ class SmokeByTimestamp(generics.ListEndpoint):
         queryset = super().get_queryset()
         return queryset.order_by('-observation_time')
     
-    
-
 
 # Will return today's smokes between times indicated by user according to the most recent observation.
 class StartEndFilter(generics.ListEndpoint):
@@ -85,7 +87,7 @@ class StartEndFilter(generics.ListEndpoint):
         start = self.request.GET.get('start')
         end = self.request.GET.get('end')
 
-        dt = datetime.now(timezone.utc)
+        dt = currentTime()
         year = dt.year
         day = dt.timetuple().tm_yday
         
@@ -96,7 +98,7 @@ class StartEndFilter(generics.ListEndpoint):
             Start = start,
             End = end,
         )
-        queryset = query_timefilter(cleaned["Start"], cleaned["End"])
+        queryset = timefilter(cleaned["Start"], cleaned["End"])
         return queryset.order_by("-end")
         
         
