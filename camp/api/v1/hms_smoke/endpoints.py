03300e134f0534343362be9e583b8e89d4801e73
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-06-11 15:47:48 -0400

    FInished Debug/Test
    Tested 3 views, will possibly add a filter test to the detail view.

--- camp/api/v1/hms_smoke/endpoints.py
+++ camp/api/v1/hms_smoke/endpoints.py
@@ -18,25 +18,24 @@ class SmokeMixin:
 
 class SmokeList(SmokeMixin, generics.ListEndpoint):
     filter_class = SmokeFilter
-
+    
+    
+class SmokeListOngoing(SmokeMixin, generics.ListEndpoint):
+    filter_class = SmokeFilter
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        curr_time = timezone.now()
+        latest_max = Smoke.objects.aggregate(Max('created'))['created__max']
+        latest_range = latest_max - timedelta(minutes=1)
+        queryset = queryset.filter(start__lte=curr_time, end__gte=curr_time, created__lte=latest_max, created__gte=latest_range)
+        return queryset
+      
+        
 class SmokeDetail(SmokeMixin, generics.DetailEndpoint):
     lookup_field = 'id'
     lookup_url_kwarg = 'smoke_id'
 
-class SmokeListOngoing(SmokeMixin, generics.ListEndpoint):
-    filter_class = SmokeFilter
-    
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        #prev_query = timezone.now() - timedelta(minutes=59, seconds=59)
-        curr_time = timezone.now()
-        print("CURR: ",curr_time)
-        #print("PREV: ",prev_query)
-        latest_max = Smoke.objects.aggregate(Max('created'))['created__max']
-        
-        queryset = queryset.filter(start__lte=curr_time, end__gte=curr_time, created=latest_max)
-        return queryset
-        
+
 
 #rm
 # env = os.environ.get
