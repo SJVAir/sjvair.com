86e830e34a3782508953b1cef82f2491c8e444cd
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-07-09 13:00:11 -0400

    FIxed smoke tests, admin, + model

--- camp/apps/integrate/hms_smoke/admin.py
+++ camp/apps/integrate/hms_smoke/admin.py
@@ -4,6 +4,11 @@ from .models import Smoke
 @admin.register(Smoke)
 class SmokeAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
-    list_filters = ['satellite', 'density', 'is_final']
+    list_filter = ['satellite', 'density', 'is_final']
     readonly_fields = ['id', 'satellite', 'density', 'is_final', 'start', 'end', 'geometry']
-    can_delete = False
+    
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
