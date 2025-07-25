11f81e370e89ed4407b12511cd24691f9ac86c88
dmpayton <derek.payton@gmail.com>
2025-07-25 10:42:27 -0700

    Add monitor_type to calibration sites report.

--- camp/apps/monitors/management/commands/closest_calibration_sites.py
+++ camp/apps/monitors/management/commands/closest_calibration_sites.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
 
         writer = csv.writer(self.stdout)
         writer.writerow([
-            'monitor_id', 'monitor_name', 'location_id', 'monitor_lat', 'monitor_lon',
+            'monitor_id', 'monitor_name', 'location_id', 'monitor_type', 'monitor_lat', 'monitor_lon',
             'ref_1_id', 'ref_1_name', 'ref_1_lat', 'ref_1_lon', 'ref_1_distance',
             'ref_2_id', 'ref_2_name', 'ref_2_lat', 'ref_2_lon', 'ref_2_distance',
             'ref_3_id', 'ref_3_name', 'ref_3_lat', 'ref_3_lon', 'ref_3_distance',
@@ -40,6 +40,7 @@ class Command(BaseCommand):
                 monitor.id,
                 monitor.name,
                 location_id,
+                monitor.monitor_type,
                 monitor.position.y,
                 monitor.position.x,
             ]
