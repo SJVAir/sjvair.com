6bc79ee4c45aa5094c198063190396befc0c91a8
Jonathan Morgan <jonmorgan1717@gmail.com>
2025-08-07 17:33:40 -0400

    HMS SMOKE: Add get day map method
    Adds an admin method to display the smokes found on the same day as the current smoke. This is only if there are other smokes.

--- camp/apps/integrate/hms_smoke/admin.py
+++ camp/apps/integrate/hms_smoke/admin.py
@@ -11,13 +11,12 @@ from camp.utils import maps
 @admin.register(Smoke)
 class SmokeAdmin(OSMGeoAdmin):
     date_hierarchy = 'date'
-    readonly_fields = ['id', 'satellite', 'density', 'date', 'start', 'end', 'is_final','get_map',]
     list_display = ['id', 'satellite', 'density', 'date', 'start', 'end', 'is_final',]
     list_filter = ['satellite', 'density', 'is_final', ]
     ordering = ('-date', )
     
     def get_fieldsets(self, request, obj=None):
-        fields = [f.name for f in self.model._meta.get_fields() if f.name != 'geometry'] + ['get_map']
+        fields = [f.name for f in self.model._meta.get_fields() if f.name != 'geometry'] + ['get_map', 'get_day_map']
         return [(None, {'fields': fields})]
     
     def has_add_permission(self, request, obj=None):
@@ -51,5 +50,32 @@ class SmokeAdmin(OSMGeoAdmin):
         ))
         content = b64encode(static_map.render(format='png')).decode()
         return mark_safe(f'<img src="data:image/png;base64,{content}" data-key="{instance.pk} "Map" />')
-    get_map.short_description = 'Map'
+    get_map.short_description = 'Smoke Map'
     
+    def get_day_map(self, instance):
+        if not instance or not instance.geometry:
+            return '-'
+        width, height = (1000, 600)
+        date_smokes = self.model.objects.filter(date=instance.date)
+        if len(date_smokes) <=1:
+            return 'This is the only smoke for the day.'
+        static_map = maps.StaticMap(
+            width=width,
+            height=height,
+            buffer=0.3
+        )
+        for smoke in date_smokes:
+            color = {
+                'light': 'yellow',
+                'medium': 'darkorange', 
+                'heavy': 'maroon'
+            }[smoke.density]
+            static_map.add(maps.Area(
+                geometry=smoke.geometry,
+                fill_color=color,
+                border_color='black',
+            ))
+        content = b64encode(static_map.render(format='png')).decode()
+        return mark_safe(f'<img src="data:image/png;base64,{content}" data-key="{instance.date} "All Plumes This Day" />')
+    get_day_map.short_description = 'All Plumes This Day'
+            
