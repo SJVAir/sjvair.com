390c7ba40b3ff6592941becec9196216f4ca840e
dmpayton <derek.payton@gmail.com>
2025-08-24 21:50:06 -0700

    Simplify and streamline AirNow client and entry imports, allowing for historical data. Also update chunk_date_range to work with datetimes.

--- camp/apps/monitors/airnow/management/commands/import_airnow_data.py
+++ camp/apps/monitors/airnow/management/commands/import_airnow_data.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from camp.apps.monitors.airnow.tasks import import_airnow_data, import_airnow_data_legacy
+from camp.apps.monitors.airnow.tasks import import_airnow_data
 
 
 class Command(BaseCommand):
@@ -8,4 +8,3 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         import_airnow_data.call_local()
-        import_airnow_data_legacy.call_local()
