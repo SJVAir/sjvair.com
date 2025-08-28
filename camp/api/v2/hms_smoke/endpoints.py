a979075940bbd235ee4500d725cb30305a48572d
dmpayton <derek.payton@gmail.com>
2025-08-27 17:39:03 -0700

    Update HMS Smoke ongoing endpoint to only fetch smoke for today, and remove the filters based on start/end.

--- camp/api/v2/hms_smoke/endpoints.py
+++ camp/api/v2/hms_smoke/endpoints.py
@@ -1,17 +1,19 @@
 from resticus import generics
 
+from django.conf import settings
 from django.utils import timezone
+
+from camp.apps.integrate.hms_smoke.models import Smoke
 
 from .filters import SmokeFilter
 from .serializers import SmokeSerializer
-from camp.apps.integrate.hms_smoke.models import Smoke
 
 
 class SmokeMixin:
     model = Smoke
     serializer_class = SmokeSerializer
     paginate = True  #defaults to page_size=100
-    def get_queryset(self):    
+    def get_queryset(self):
         return (
             super().get_queryset().order_by('-date')
         )
@@ -19,22 +21,17 @@ class SmokeMixin:
 
 class SmokeList(SmokeMixin, generics.ListEndpoint):
     filter_class = SmokeFilter
-    
-    
+
+
 class SmokeListOngoing(SmokeMixin, generics.ListEndpoint):
     filter_class = SmokeFilter
-    
+
     def get_queryset(self):
-        #Gets from the latest smoke data + ongoing smokes
-        now = timezone.now()
-        return (
-            super().get_queryset()
-                .filter(start__lte=now, end__gte=now, date=now.date(),)
-                .order_by('end')
-        )
+        # Return smoke for today.
+        today = timezone.now().astimezone(settings.DEFAULT_TIMEZONE).date()
+        return super().get_queryset().filter(date=today)
 
 
 class SmokeDetail(SmokeMixin, generics.DetailEndpoint):
     lookup_field = 'id'
     lookup_url_kwarg = 'smoke_id'
-        
