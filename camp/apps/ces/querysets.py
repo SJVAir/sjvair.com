fd5d61d7755bb081280df477e7012bb60d03f38f
dmpayton <derek.payton@gmail.com>
2026-03-10 23:34:36 -0700

    Fix CESQuerySet (use Manager), serialize dac_category as display value, fix duplicate fixture key

--- camp/apps/ces/querysets.py
+++ camp/apps/ces/querysets.py
@@ -2,11 +2,19 @@ from django.db import models
 
 
 class CESQuerySet(models.QuerySet):
-    def get_queryset(self):
-        return super().get_queryset().select_related('boundary__region')
-
     def for_version(self, version):
         return self.filter(boundary__version=version)
 
     def for_tract(self, geoid):
         return self.filter(boundary__region__external_id=geoid)
+
+
+class CESManager(models.Manager):
+    def get_queryset(self):
+        return CESQuerySet(self.model, using=self._db).select_related('boundary__region')
+
+    def for_version(self, version):
+        return self.get_queryset().for_version(version)
+
+    def for_tract(self, geoid):
+        return self.get_queryset().for_tract(geoid)
