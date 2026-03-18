b24d34720fcdb0863105d157715f287af696ef57
dmpayton <derek.payton@gmail.com>
2026-03-18 10:11:09 -0700

    feat(ceidars): add app skeleton

--- camp/settings/base.py
+++ camp/settings/base.py
@@ -112,6 +112,7 @@ INSTALLED_APPS = [
     'camp.apps.entries',
     'camp.apps.helpdesk',
     'camp.apps.integrate.hms_smoke',
+    'camp.apps.ceidars',
     'camp.apps.monitors',
     'camp.apps.monitors.airgradient',
     'camp.apps.monitors.airnow',
