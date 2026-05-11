17fbf3889c2f1fb109577477ca770b81c29bbc22
dmpayton <derek.payton@gmail.com>
2026-05-11 00:40:03 -0700

    chore(pesticides): Remove stale migration and rename periodic task

--- camp/apps/pesticides/tasks.py
+++ camp/apps/pesticides/tasks.py
@@ -3,6 +3,6 @@ from huey import crontab
 
 
 @db_periodic_task(crontab(minute='0', hour='14'), priority=50)
-def fetch_spray_applications():
+def fetch_pesticide_notices():
     from camp.apps.pesticides.spraydays import fetch_applications
     fetch_applications()
