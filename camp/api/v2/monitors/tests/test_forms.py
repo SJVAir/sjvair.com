eb77a3f6914ae36fa738a98e8be231e06bc7bdff
dmpayton <derek.payton@gmail.com>
2026-07-06 15:59:03 -0700

    test: strengthen MonitorAtForm timestamp localization coverage

--- camp/api/v2/monitors/tests/test_forms.py
+++ camp/api/v2/monitors/tests/test_forms.py
@@ -1,9 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timezone as dt_timezone
 
+from django.conf import settings
 from django.test import TestCase
 from django.utils import timezone
 
 from camp.api.v2.monitors.forms import MonitorAtForm
+from camp.utils.datetime import make_aware
 
 
 class MonitorAtFormTests(TestCase):
@@ -15,7 +17,17 @@ class MonitorAtFormTests(TestCase):
     def test_naive_timestamp_assumed_local(self):
         form = MonitorAtForm(data={'timestamp': '2026-07-04 21:00:00'})
         assert form.is_valid(), form.errors
+        expected = make_aware(datetime(2026, 7, 4, 21, 0, 0), tz=settings.DEFAULT_TIMEZONE)
         assert timezone.is_aware(form.cleaned_data['timestamp'])
+        assert form.cleaned_data['timestamp'] == expected
+
+    def test_aware_timestamp_passed_through_unchanged(self):
+        form = MonitorAtForm(data={'timestamp': '2026-07-04T21:00:00Z'})
+        assert form.is_valid(), form.errors
+        expected = datetime(2026, 7, 4, 21, 0, 0, tzinfo=dt_timezone.utc)
+        assert timezone.is_aware(form.cleaned_data['timestamp'])
+        assert form.cleaned_data['timestamp'] == expected
+        assert form.cleaned_data['timestamp'].timestamp() == expected.timestamp()
 
     def test_bbox_parses_four_floats(self):
         form = MonitorAtForm(data={
