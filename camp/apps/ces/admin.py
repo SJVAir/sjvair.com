f7c39ca6e526ae77d903e3276cd1e3c4af47aa0e
dmpayton <derek.payton@gmail.com>
2026-07-11 15:37:31 -0700

    feat(ces): register CES5 admin

--- camp/apps/ces/admin.py
+++ camp/apps/ces/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-from camp.apps.ces.models import CES4
+from camp.apps.ces.models import CES4, CES5
 
 
 class CESRecordAdmin(admin.ModelAdmin):
@@ -67,6 +67,64 @@ class CES4Admin(CESRecordAdmin):
                 'pop_under_10', 'pop_10_64', 'pop_65_plus',
                 'pop_hispanic', 'pop_white', 'pop_black',
                 'pop_native', 'pop_aapi', 'pop_other',
+            ],
+            'classes': ['collapse'],
+        }),
+    ]
+
+
+@admin.register(CES5)
+class CES5Admin(CESRecordAdmin):
+    fieldsets = [
+        (None, {
+            'fields': [
+                'tract', 'census_year', 'region', 'zipcode', 'approx_loc',
+                'county', 'region_name', 'population', 'ci_score', 'ci_score_p',
+            ],
+        }),
+        (_('SB535 Disadvantaged Community'), {
+            'fields': ['dac_sb535', 'dac_category'],
+        }),
+        (_('Pollution Burden'), {
+            'fields': [
+                'pollution', 'pollution_s', 'pollution_p',
+                'pol_ozone', 'pol_ozone_p',
+                'pol_pm', 'pol_pm_p',
+                'pol_diesel', 'pol_diesel_p',
+                'pol_pest', 'pol_pest_p',
+                'pol_rsei_haz', 'pol_rsei_haz_p',
+                'pol_traffic', 'pol_traffic_p',
+                'pol_drink', 'pol_drink_p',
+                'pol_lead', 'pol_lead_p',
+                'pol_cleanups', 'pol_cleanups_p',
+                'pol_gwthreats', 'pol_gwthreats_p',
+                'pol_haz', 'pol_haz_p',
+                'pol_iwb', 'pol_iwb_p',
+                'pol_small_ats', 'pol_small_ats_p',
+                'pol_swis', 'pol_swis_p',
+            ],
+            'classes': ['collapse'],
+        }),
+        (_('Population Characteristics'), {
+            'fields': [
+                'popchar', 'popchar_s', 'popchar_p',
+                'char_asthma', 'char_asthma_p',
+                'char_cvd', 'char_cvd_p',
+                'char_diabetes', 'char_diabetes_p',
+                'char_lbw', 'char_lbw_p',
+                'char_edu', 'char_edu_p',
+                'char_ling', 'char_ling_p',
+                'char_pov', 'char_pov_p',
+                'char_unemp', 'char_unemp_p',
+                'char_housingb', 'char_housingb_p',
+            ],
+            'classes': ['collapse'],
+        }),
+        (_('Demographics'), {
+            'fields': [
+                'pop_under_10_pct', 'pop_10_64_pct', 'pop_65_plus_pct',
+                'pop_hispanic_pct', 'pop_white_pct', 'pop_black_pct',
+                'pop_native_pct', 'pop_asian_pct', 'pop_pacisl_pct', 'pop_other_pct',
             ],
             'classes': ['collapse'],
         }),
