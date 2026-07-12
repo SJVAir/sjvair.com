d8fe0634da8f48dca402560b992f36cbbb25cda1
dmpayton <derek.payton@gmail.com>
2026-07-11 19:37:51 -0700

    feat: add CIMISAPI client for the CIMIS REST API

---
+++ camp/apps/monitors/cimis/api.py
@@ -0,0 +1,31 @@
+import requests
+
+from django.conf import settings
+
+
+class CIMISAPI:
+    base_url = 'https://et.water.ca.gov/api'
+
+    def __init__(self, app_key=None):
+        self.app_key = app_key or settings.CIMIS_APP_KEY
+        self.session = requests.Session()
+
+    def get_stations(self):
+        response = self.session.get(f'{self.base_url}/station', params={
+            'appKey': self.app_key,
+        })
+        response.raise_for_status()
+        return response.json()['Stations']
+
+    def get_hourly_data(self, station_numbers, start_date, end_date, data_items):
+        params = {
+            'appKey': self.app_key,
+            'targets': ','.join(str(n) for n in station_numbers),
+            'startDate': start_date.strftime('%Y-%m-%d'),
+            'endDate': end_date.strftime('%Y-%m-%d'),
+            'dataItems': ','.join(data_items),
+            'unitOfMeasure': 'E',
+        }
+        response = self.session.get(f'{self.base_url}/data', params=params)
+        response.raise_for_status()
+        return response.json()['Data']['Providers']
--- camp/apps/monitors/cimis/tests.py
+++ camp/apps/monitors/cimis/tests.py
@@ -1,7 +1,10 @@
+from unittest.mock import MagicMock, patch
+
 from django.contrib.gis.geos import Point
 from django.test import TestCase
 
 from camp.apps.entries import models as entry_models
+from camp.apps.monitors.cimis.api import CIMISAPI
 from camp.apps.monitors.cimis.models import CIMIS
 
 
@@ -34,3 +37,54 @@ class CIMISModelTests(TestCase):
             location=CIMIS.LOCATION.outside,
         )
         assert monitor.supports_health_checks() is False
+
+
+def make_response(status_code=200, json_result=None):
+    response = MagicMock()
+    response.status_code = status_code
+    response.json.return_value = json_result
+    response.raise_for_status = MagicMock()
+    if status_code >= 400:
+        response.raise_for_status.side_effect = Exception(f'HTTP {status_code}')
+    return response
+
+
+class CIMISAPITests(TestCase):
+    def setUp(self):
+        self.api = CIMISAPI(app_key='test-key')
+
+    @patch('camp.apps.monitors.cimis.api.requests.Session.get')
+    def test_get_stations_returns_station_list(self, mock_get):
+        mock_get.return_value = make_response(json_result={'Stations': [{'StationNbr': '2'}]})
+
+        stations = self.api.get_stations()
+
+        assert stations == [{'StationNbr': '2'}]
+        called_url, called_kwargs = mock_get.call_args
+        assert called_url[0] == 'https://et.water.ca.gov/api/station'
+        assert called_kwargs['params']['appKey'] == 'test-key'
+
+    @patch('camp.apps.monitors.cimis.api.requests.Session.get')
+    def test_get_hourly_data_builds_correct_params(self, mock_get):
+        mock_get.return_value = make_response(json_result={
+            'Data': {'Providers': [{'Name': 'cimis', 'Records': []}]}
+        })
+
+        from datetime import date
+        providers = self.api.get_hourly_data(
+            station_numbers=['2', '5'],
+            start_date=date(2026, 7, 1),
+            end_date=date(2026, 7, 1),
+            data_items=['hly-air-tmp', 'hly-wind-spd'],
+        )
+
+        assert providers == [{'Name': 'cimis', 'Records': []}]
+        called_url, called_kwargs = mock_get.call_args
+        assert called_url[0] == 'https://et.water.ca.gov/api/data'
+        params = called_kwargs['params']
+        assert params['targets'] == '2,5'
+        assert params['startDate'] == '2026-07-01'
+        assert params['endDate'] == '2026-07-01'
+        assert params['dataItems'] == 'hly-air-tmp,hly-wind-spd'
+        assert params['unitOfMeasure'] == 'E'
+        assert params['appKey'] == 'test-key'
