9b2a8530a27ae36bcd9892ad49403fe8661c3c4d
dmpayton <derek.payton@gmail.com>
2026-07-12 20:27:03 -0700

    feat(vozbox): add VOZBox to device filter in v1 and v2 APIs

--- camp/api/v2/monitors/filters.py
+++ camp/api/v2/monitors/filters.py
@@ -10,10 +10,11 @@ class MonitorFilter(FilterSet):
 
     def filter_device(self, queryset, name, value):
         lookup_field = {
-            'PurpleAir': 'purpleair',
             'AirNow': 'airnow',
             'AQview': 'aqview',
             'BAM1022': 'bam1022',
+            'PurpleAir': 'purpleair',
+            'VOZBox': 'vozbox',
         }.get(value)
 
         if lookup_field is not None:
